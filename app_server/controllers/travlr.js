a8863a6ab74b19e9a24d6c15eeafb81e860fd7a0
Ritu <riuthuma@gmail.com>
2026-05-23 18:32:11 -0500

    Module 3 -Static HTML to JSON with HBS templates

--- app_server/controllers/travlr.js
+++ app_server/controllers/travlr.js
@@ -1,56 +1,59 @@
+var fs = require('fs');
+
 /* GET travel page */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travel' });
-  };
-  
-  /* GET news page */
-  const news = (req, res) => {
-    res.render('news', { title: 'News' });
-  };
-  
-  /* GET reservations page */
-  const reservations = (req, res) => {
-    res.render('reservations', { title: 'Reservations' });
-  };
-  
-  /* GET login page */
-  const login = (req, res) => {
-    res.render('login', { title: 'Login' });
-  };
-  
-  /* GET admin page */
-  const admin = (req, res) => {
-    res.render('admin', { title: 'Admin' });
-  };
-  
-  /* GET rooms page */
-  const rooms = (req, res) => {
-    res.render('rooms', { title: 'Rooms' });
-  };
-  
-  /* GET meals page */
-  const meals = (req, res) => {
-    res.render('meals', { title: 'Meals' });
-  };
-  
-  /* GET about page */
-  const about = (req, res) => {
-    res.render('about', { title: 'About' });
-  };
-  
-  /* GET contact page */
-  const contact = (req, res) => {
-    res.render('contact', { title: 'Contact' });
-  };
-  
-  module.exports = {
-    travel,
-    news,
-    reservations,
-    login,
-    admin,
-    rooms,
-    meals,
-    about,
-    contact
-  };
+  var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+  res.render('travel', { title: 'Travlr', trips: trips });
+};
+
+/* GET news page */
+const news = (req, res) => {
+  res.render('news', { title: 'News' });
+};
+
+/* GET reservations page */
+const reservations = (req, res) => {
+  res.render('reservations', { title: 'Reservations' });
+};
+
+/* GET login page */
+const login = (req, res) => {
+  res.render('login', { title: 'Login' });
+};
+
+/* GET admin page */
+const admin = (req, res) => {
+  res.render('admin', { title: 'Admin' });
+};
+
+/* GET rooms page */
+const rooms = (req, res) => {
+  res.render('rooms', { title: 'Rooms' });
+};
+
+/* GET meals page */
+const meals = (req, res) => {
+  res.render('meals', { title: 'Meals' });
+};
+
+/* GET about page */
+const about = (req, res) => {
+  res.render('about', { title: 'About' });
+};
+
+/* GET contact page */
+const contact = (req, res) => {
+  res.render('contact', { title: 'Contact' });
+};
+
+module.exports = {
+  travel,
+  news,
+  reservations,
+  login,
+  admin,
+  rooms,
+  meals,
+  about,
+  contact
+};
